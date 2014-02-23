5440e0a513ae80a3247a3fb8376a6365027d9267
banjun <banjun@gmail.com>
2014-02-23 13:45:18 +0900

    add example project

---
+++ librtmpExample/librtmpExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  librtmpExample
+//
+//  Created by banjun on 2014/02/23.
+//  Copyright (c) 2014年 banjun. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ librtmpExample/librtmpExample/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  librtmpExample
+//
+//  Created by banjun on 2014/02/23.
+//  Copyright (c) 2014年 banjun. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
